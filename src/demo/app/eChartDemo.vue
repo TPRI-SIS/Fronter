3ab3a4e282c55dfb2758a830e6dfeb0573424dc4
BeiDream <331341164@qq.com>
2017-05-17 08:57:38 +0800

    格式化代码

--- src/demo/app/eChartDemo.vue
+++ src/demo/app/eChartDemo.vue
@@ -1,68 +1,70 @@
 <template>
     <div class="main">
-    <gesture :swipeRight="swipeRight">
-        <mu-raised-button class="demo-raised-button" icon="android" backgroundColor="#a4c639" @click="changeTitle"/>
-    <vueEchart :options="options" :mystyle="nowstyle"></vueEchart>
-
-    </gesture>
+        <gesture :swipeRight="swipeRight">
+            <mu-raised-button class="demo-raised-button" icon="android" backgroundColor="#a4c639" @click="changeTitle" />
+            <vueEchart :options="options" :mystyle="nowstyle"></vueEchart>
+        </gesture>
     </div>
 </template>
 
 <script>
-var list=[5, 20, 36, 10, 10, 20];
-export default {
-    name: 'eChartDemo',
-    data() {
-        return {
-            contentStyle: {
-                height: this.$screen.height
-            },
-            nowstyle: {
-                width: '300px',
-                height: '400px'   
-            },
-            options:{
-                title: { text: 'Vue---ECharts' },
-                tooltip: {},
-                xAxis: {
-                    data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+    var list = [5, 20, 36, 10, 10, 20];
+    export default {
+        name: 'eChartDemo',
+        data() {
+            return {
+                contentStyle: {
+                    height: this.$screen.height
                 },
-                yAxis: {},
-                series: [{
-                    name: '销量',
-                    type: 'bar',
-                    data: [5, 20, 36, 10, 10, 20]
-                }]
-            },
-            swipeRight: function (e) {
-                this.$router.go(-1)
-            }
-        }
-    },
-    methods:{
-        changeTitle:function(){
-            this.options={
-                title: { text: 'Vue---ECharts' },
-                tooltip: {},
-                xAxis: {
-                    data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子","毛衣"]
+                nowstyle: {
+                    width: '300px',
+                    height: '400px'
                 },
-                yAxis: {},
-                series: [{
-                    name: '销量',
-                    type: 'bar',
-                    data: [5, 20, 36, 10, 10, 20,50]
-                }]
+                options: {
+                    title: {
+                        text: 'Vue---ECharts'
+                    },
+                    tooltip: {},
+                    xAxis: {
+                        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+                    },
+                    yAxis: {},
+                    series: [{
+                        name: '销量',
+                        type: 'bar',
+                        data: [5, 20, 36, 10, 10, 20]
+                    }]
+                },
+                swipeRight: function(e) {
+                    this.$router.go(-1)
+                }
             }
-
+        },
+        methods: {
+            changeTitle: function() {
+                this.options = {
+                    title: {
+                        text: 'Vue---ECharts'
+                    },
+                    tooltip: {},
+                    xAxis: {
+                        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子", "毛衣"]
+                    },
+                    yAxis: {},
+                    series: [{
+                        name: '销量',
+                        type: 'bar',
+                        data: [5, 20, 36, 10, 10, 20, 50]
+                    }]
+                }
+            }
         }
     }
-}
 </script>
 
 <style scoped>
-.main {
-    background: #95D1DF;
-    height: 100%;
-}
+    .main {
+        background: #95D1DF;
+        height: 100%;
+    }
 </style>
--- src/framework/ui/vueEchart.vue
+++ src/framework/ui/vueEchart.vue
@@ -8,55 +8,52 @@
     var myChart;
     export default {
         name: 'vueEchart',
-        data(){
-            return{
-                styleObject:this.mystyle
+        data() {
+            return {
+                styleObject: this.mystyle
             }
         },
-        props: {   
+        props: {
             options: {
                 type: Object,
-                default: function () {
-                        return {
-                                
-                        }
+                default: function() {
+                    return {}
                 }
             },
             mystyle: {
                 type: Object,
-                default: function () {
-                        return {
-                            width: '800px',
-                            height: '400px'     
-                        }
+                default: function() {
+                    return {
+                        width: '800px',
+                        height: '400px'
+                    }
                 }
-
             }
         },
-        mounted: function () {
+        mounted: function() {
             myChart = echarts.init(document.getElementById('chart'));
             // 绘制图表
             myChart.setOption(this.options);
         },
-        watch: {  
-            options:{
-　　　　　　　　　//注意：当观察的数据为对象或数组时，curVal和oldVal是相等的，因为这两个形参指向的是同一个数据对象
-　　　　　　　　　　handler(curVal,oldVal){
-　　　　　　　　　　　　 this.refresh();
-　　　　　　　　　　},
-　　　　　　　　　　deep:true
-　　　　　　　　} 
+        watch: {
+            options: {　　　　　　　　　 //注意：当观察的数据为对象或数组时，curVal和oldVal是相等的，因为这两个形参指向的是同一个数据对象
+                　　　　　　　　　　
+                handler(curVal, oldVal) {　　　　　　　　　　　　
+                    this.refresh();　　　　　　　　　　
+                },
+                　　　　　　　　　　deep: true　　　　　　　　
+            }
         },
-        methods:{
-            refresh:function(){
+        methods: {
+            refresh: function() {
                 myChart.setOption(this.options);
             }
         }
     }
 </script>
 <style scoped>
-.main{
-    background:green;
-      height:100%
-}
+    .main {
+        background: green;
+        height: 100%
+    }
 </style>
