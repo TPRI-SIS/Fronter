8ce08d89f226e51e0bfadf391ad3c2a3fb9ebe4a
creatworld <creatworld@163.com>
2017-05-23 11:15:18 +0800

    aa

--- src/demo/app/navLayoutDemo.vue
+++ src/demo/app/navLayoutDemo.vue
@@ -1,50 +1,92 @@
 <template>
   <navLayout :open="open" :trigger="handleTrigger">
     <div slot="nav">
-      <mu-appbar title="open">
-  
+      <mu-appbar title="功能列表" class="appBar">  
       </mu-appbar>
+      <scroll class="scroll" :style='{height:scrollHeight}' :height='(list.length+2)*48+"px"'>
+        <mu-list>
+          <mu-list-item v-for="item in list" :title="item.name" class="item" @click="changeContent(item.name)">
+          </mu-list-item>
+        </mu-list>
+      </scroll>
     </div>
     <div class="content" slot="content">
-      <mu-appbar title="Title" class="appBar">
-        <mu-icon-button icon="menu" slot="left" />
-        <mu-flat-button label="expand_more" slot="right" />
-        <mu-flat-button  label="返回" slot="right" @click='openOrClose' />
-        <mu-icon-button icon="expand_more" slot="right" />
+      <mu-appbar :title="contentTitle" class="appBar">
+        <mu-icon-button icon="arrow_back_ios" slot="left" @click='back' />
+        <mu-icon-button icon="expand_more" ref="button" slot="right" @click="toggle"/>
+       
       </mu-appbar>
+       <mu-popover :trigger="menuTrigger" :open="menuOpen" @close="handleClose">
+          <mu-menu>
+            <mu-menu-item title="选择1" @click="showToast"/>
+            <mu-menu-item title="选择2" />
+            <mu-menu-item title="选择3" />
+            <mu-menu-item title="选择4" />
+            <mu-menu-item title="选择5" />
+          </mu-menu>
+        </mu-popover>
       <div class="appContent">
-        <mu-raised-button label="Primary" class="demo-raised-button" primary @click="openOrClose" />
+        <p>{{contentTitle}}</p>
       </div>
-  
+      <mu-toast v-if="toast" message="一段简单的文本" />
     </div>
   </navLayout>
 </template>
 <script>
+
+var l = []
+for (var i = 0; i < 33; i++) {
+  var item = { name: '应用功能' + i }
+  l.push(item)
+}
+
 export default {
   name: 'navLayoutDemo',
   data() {
     return {
-      open: false
+      open: false,
+      contentTitle: '',
+      scrollHeight: $screen.height + 'px',
+      list: l,
+      menuOpen: false,
+      menuTrigger: null,
+      toast:false
     }
   },
-  watch:{
-    open:function(value){
+  watch: {
+    open: function (value) {
       console.log(value)
     }
   },
   methods: {
-    openOrClose: function () {
-
-      this.$router.go(-1)
-    },
     handleTrigger: function (e) {
 
+    },
+    back: function () {
+      this.$router.go(-1)
+    },
+    changeContent: function (e) {
+      this.contentTitle = e
+    },
+    showToast:function(){
+      this.toast=true
+      var vm=this
+      setTimeout(() => { vm.toast = false }, 2000)
+    },
+    toggle () {
+      this.menuOpen = !this.open
+    },
+    handleClose (e) {
+      this.menuOpen = false
     }
   },
-  activated(){
-      //当窗口激活时加载数据
+   mounted () {
+    this.menuTrigger = this.$refs.button.$el
   },
-  deactivated(){
+  activated() {
+    //当窗口激活时加载数据
+  },
+  deactivated() {
     //当窗口为非激活时做一些东西，例如停止计时器之类
   }
 }
@@ -56,10 +98,19 @@ export default {
   width: 100%;
 }
 
-.appContent{
-    position: absolute;
+.appContent {
+  position: absolute;
   width: 100%;
-  top:64px;
+  top: 64px;
+}
+
+.scroll{
+  padding-top:64px;
+}
+
+.item {
+  text-align: left;
+  padding-left: 48px;
 }
 
 .main {
