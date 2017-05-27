a936b38d93f756984d47a347c193275b392ae99c
creatworld <creatworld@163.com>
2017-05-27 16:25:37 +0800

    增加 下拉刷新的scroll

--- src/demo/app/scrollExDemo.vue
+++ src/demo/app/scrollExDemo.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <scroll-ex :style="{height:mainScrollHeight}" :upOpenClosed="handlerUp"  :downOpenClosed="handlerDown" :width="width" :height="height">
+    <scroll-ex :style="{height:mainScrollHeight}" :upOpenClosed="handlerUp" :downOpenClosed="handlerDown" :width="width"
+               :height="height">
       <mu-list style="padding: 0px">
         <mu-list-item style="background-color: dodgerblue" v-for="item in list" :title="item.name" class="item"
                       :key="item.name">
@@ -28,7 +29,22 @@
   import ScrollEx from "../../framework/ui/scrollEx";
 
 
-  var source= [
+  var source = [
+    {
+      name: "test1"
+    },
+    {
+      name: "test2"
+    },
+    {
+      name: "test3"
+    },
+    {
+      name: "test4"
+    },
+    {
+      name: "test5"
+    },
     {
       name: "test1"
     },
@@ -84,22 +100,22 @@
         width: $screen.width + 'px',
         height: source.length * 48 + 'px',
         mainScrollHeight: $screen.height + 'px',
-        list:source
+        list: source
 
       }
     },
     methods: {
-        handlerUp:function () {
-           this.list=source.slice()
-          this.height= this.list.length * 48 + 'px'
-        },
-        handlerDown:function(){
+      handlerUp: function () {
+        this.list = source.slice()
+        this.height = this.list.length * 48 + 'px'
+      },
+      handlerDown: function () {
 
-            for(var i=11;i<20;i++){
-                this.list.push({name:'test'+new Date()})
-            }
-            this.height= this.list.length * 48 + 'px'
+        for (var i = 11; i < 20; i++) {
+          this.list.push({name: 'test' + new Date()})
         }
+        this.height = this.list.length * 48 + 'px'
+      }
 
     },
     beforeCreate(){
